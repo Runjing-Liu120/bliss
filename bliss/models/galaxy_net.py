d3364246513a271a629903ba0658024770b511f5
Ismael Mendoza <imendoza@umich.edu>
2021-04-14 12:41:43 -0400

    residual blocks

--- bliss/models/galaxy_net.py
+++ bliss/models/galaxy_net.py
@@ -7,10 +7,79 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.distributions import Normal
 from torch.nn import L1Loss
+from torch.nn.modules.conv import Conv2d
 
 from bliss.optimizer import get_optimizer
 
 plt.switch_backend("Agg")
+
+
+def activation_func(activation):
+    return nn.ModuleDict(
+        [
+            ["relu", nn.ReLU(inplace=True)],
+            ["leaky_relu", nn.LeakyReLU(negative_slope=0.01, inplace=True)],
+            ["selu", nn.SELU(inplace=True)],
+            ["none", nn.Identity()],
+        ]
+    )[activation]
+
+
+def conv_bn(in_channels, out_channels, stride=1):
+    return nn.Sequential(
+        Conv2d(in_channels, out_channels, kernel_size=3, padding=1, stride=stride),
+        nn.BatchNorm2d(out_channels),
+    )
+
+
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, activation="relu"):
+        super().__init__()
+        self.in_channels, self.out_channels, self.activation = in_channels, out_channels, activation
+        self.blocks = nn.Identity()
+        self.activate = activation_func(activation)
+        self.shortcut = nn.Identity()
+
+    def forward(self, x):
+        residual = x
+        if self.should_apply_shortcut:
+            residual = self.shortcut(x)
+        x = self.blocks(x)
+        x += residual
+        x = self.activate(x)
+        return x
+
+    @property
+    def should_apply_shortcut(self):
+        return self.in_channels != self.out_channels
+
+
+class ResNetResidualBlock(ResidualBlock):
+    def __init__(self, in_channels, out_channels, expansion=1, downsampling=1, conv=None, **kwargs):
+        super().__init__(in_channels, out_channels, **kwargs)
+        self.expansion, self.downsampling, self.conv = expansion, downsampling, conv
+        self.shortcut = (
+            nn.Sequential(
+                nn.Conv2d(
+                    self.in_channels,
+                    self.expanded_channels,
+                    kernel_size=1,
+                    stride=self.downsampling,
+                    bias=False,
+                ),
+                nn.BatchNorm2d(self.expanded_channels),
+            )
+            if self.should_apply_shortcut
+            else None
+        )
+
+    @property
+    def expanded_channels(self):
+        return self.out_channels * self.expansion
+
+    @property
+    def should_apply_shortcut(self):
+        return self.in_channels != self.expanded_channels
 
 
 class GalaxyEncoder(nn.Module):
